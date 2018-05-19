4b00be9692f08ede182cbda4e981babdd135c1a1
William Li <githuberr@outlook.com>
2018-05-19 11:44:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarfs)  # code an argument here
   # Your code here
   dwarfs.each_with_index do |item, index|
-    puts "#{index} #{item}"
+    puts "#{index+1} #{item}"
   end
 end
 
